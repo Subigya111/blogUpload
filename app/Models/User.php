33804388fbfd2bef8c265970e7a8a78dc604ee53
subigya <subigyapok08@gmail.com>
2026-05-16 23:52:25 +0545

    Day 1- made post table with title and content row and established relation with user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    protected $fillable =['name','email','password'];
 
     /**
      * Get the attributes that should be cast.
@@ -29,4 +30,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function post(){
+        return $this->hasMany(Post::class,'user_id');
+    }
+
 }
